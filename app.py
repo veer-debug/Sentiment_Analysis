cc24fb9fe4f5de440d72949b98c63a8d791b6aad
Ranveer <theveer07ranveer@gmail.com>
2024-01-06 22:00:02 +0530

    upgrade gui

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PIL import Image,ImageTk
 from tkinter import scrolledtext
 from tkinter import messagebox
 from mydb import Database
+from myapi import API
 # from sign_up import Signup
 
 
@@ -11,12 +12,15 @@ from mydb import Database
 class Login:
     def __init__(self,root):
         self.dob=Database()
+        self.api=API()
         # self.signup=Signup(root)
         self.root=root
         self.root.geometry("1500x780")
         self.root.title("Login Page")
         self.root.iconbitmap(r"C:\Users\theve\OneDrive\Desktop\git_projects\Sentiment_Analysis\resorces\logo.ico")
-        self.Login(root)
+        # self.Login(root)
+        self.Sentiment_analysis()
+        # self.main()
     def Login(self,root):   
         self.clear()     
         title_ibl=Label(self.root,text="Analysis",font=("times new roman",30,"bold"),bg="white",fg="red")
@@ -123,7 +127,7 @@ class Login:
         labelr=Label(Right_frame,text="Alrady have a account ",width=40,bg="light yellow",font=("times new roman",15,"bold"))
         labelr.place(x=-20,y=500)
         
-        Rigister1=Button(Right_frame,text="Login",width=10,font=("times new roman",15,"bold"),bg="green",fg="white",command=self.login1)
+        Rigister1=Button(Right_frame,text="Login",width=10,font=("times new roman",15,"bold"),bg="green",fg="white",command=self.login)
         Rigister1.place(x=325,y=500)
     # ===========================================
     def clear(self):
@@ -136,10 +140,13 @@ class Login:
         # pass
         email=self.email_input.get()
         password=self.password_input.get()
-        
+        if email=='' or password=='':
+             messagebox.showerror('Error',"Fill all Values")
+             return 
         responce=self.dob.search(email,password)    
         if responce:
             messagebox.showinfo('Success',"Login Succesful")
+            self.main()
             # self.Sign_up(root)
         else:
             messagebox.showerror('Error',"Envlid User")
@@ -154,15 +161,125 @@ class Login:
         email=self.email_input.get()
         password=self.password_input.get()
         respons=self.dob.add_data(name,email,password)
+        if email=='' or password=='' or name=='':
+             messagebox.showerror('Error',"Fill all Values")
+             return 
         
         if respons:
             messagebox.showinfo('Success',"Registration success")
             self.Login(root)
             
+            
         else:
             messagebox.showerror('Error',"Email alredy exist")
+    
+    def main(self):
+        # sentiment analysis
+        # name entry recognition
+        # emotion prediction
+        self.clear()
+        
+        title_ibl=Label(self.root,text="Analysis",font=("times new roman",30,"bold"),bg="white",fg="red")
+        title_ibl.place(x=0,y=0,width=1530,height=45)
+        
+        main_frame=Frame(root,bd=2)
+        main_frame.place(x=0,y=50,width=1600,height=1000)
         
         
+        #-----------------------------------------left lable frame---------------------------------------------------------------
+        Left_frame=LabelFrame(main_frame,bd=2,bg="light yellow",relief=RIDGE,font=("times new roman",12,"bold"))
+        Left_frame.place(x=20,y=0,width=1460,height=680)
+        
+        
+        
+        # =============================Buttans===================
+        
+        Sentiment=Button(Left_frame,text="Sentiment analysis",width=30,font=("times new roman",25,"bold"),bg="orange",fg="white",command=self.Sentiment_analysis)
+        Sentiment.place(x=100,y=100)
+        
+        Ner=Button(Left_frame,text="Name Entry Recognition",width=30,font=("times new roman",25,"bold"),bg="White",fg="black",command=self.NER_Def)
+        Ner.place(x=420,y=250)
+        
+        Emotion=Button(Left_frame,text="Emotion prediction",width=30,font=("times new roman",25,"bold"),bg="green",fg="white",command=self.Emotion_def)
+        Emotion.place(x=750,y=400)
+        
+        logout=Button(Left_frame,text="logout",width=70,font=("times new roman",25,"bold"),bg="red",fg="white",command=self.logout)
+        logout.place(x=20,y=550)
+        
+        
+    def Sentiment_analysis(self):
+        self.clear()
+        title_ibl=Label(self.root,text="Sentiment Analysis",font=("times new roman",30,"bold"),bg="white",fg="red")
+        title_ibl.place(x=0,y=0,width=1530,height=45)
+        
+        main_frame=Frame(root,bd=2)
+        main_frame.place(x=0,y=50,width=1600,height=1000)
+        
+        
+        #-----------------------------------------left lable frame---------------------------------------------------------------
+        Left_frame=LabelFrame(main_frame,bd=2,bg="light yellow",relief=RIDGE,font=("times new roman",12,"bold"))
+        Left_frame.place(x=20,y=0,width=1460,height=680)
+        
+        
+        
+        # =============================RIGHT LABLE FRAME===================
+     
+        # labeles=Label(Left_frame,text="Sentiment Analysis",width=40,bg="light yellow",fg="green",font=("times new roman",25,"bold"))
+        # labeles.pack(pady=10)
+        
+        # heading1=Label(Left_frame,text='Sentiment Analysis',bg='light Yellow',fg='green')
+        # heading1.pack(pady=(50,10))
+        # heading1.config(font=('verdana',34,'bold'))
+        
+        img=Image.open(r"C:\Users\theve\OneDrive\Desktop\git_projects\Sentiment_Analysis\resorces\logo.ico")
+        img=img.resize((80,80))
+        self.photoimg=ImageTk.PhotoImage(img)
+        
+        f_lb=Label(Left_frame,image=self.photoimg)
+        f_lb.pack(pady=(30,10))
+         
+        
+        self.labeli=Label(Left_frame,text="Enter Text",bg="light yellow")
+        self.labeli.pack(pady=20)
+        self.labeli.config(font=('verdana',34,'bold'))
+        
+        self.sentiment_input=Entry(Left_frame,width=100,bg='white',fg='red')
+        self.sentiment_input.pack(pady=(20,20),ipady=20)
+        
+        sentiment_btn=Button(Left_frame,text="Analys",width=15,bg='red',fg='white',font=("times new roman",25,"bold"),command=self.do_sentiment)
+        sentiment_btn.pack(pady=(30,30))
+        
+        
+        self.result=Label(Left_frame,text="",bg='light yellow',fg='light green')
+        self.result.pack(pady=20)
+        self.result.config(font=('verdana',16))
+        
+        goback=Button(Left_frame,text="Go Back",bg='green',fg='black',width=10,font=("times new roman",25,"bold"),command=self.main)
+        goback.pack()
+        
+        
+    def NER_Def(self):
+        pass
+        
+        
+    def Emotion_def(self):
+        pass
+    
+    
+    
+    def logout(self):
+        self.Login(self.root)  
+        
+    
+    
+    def do_sentiment(self):
+        txt=self.sentiment_input.get()
+        result=self.api.sentiment_analysis(txt)
+        txt=""
+        for i in result['sentiment']:
+            txt=txt+i+" -> "+str(result['sentiment'][i])+'\n'
+                
+        self.result['text']=txt      
         
         
         
